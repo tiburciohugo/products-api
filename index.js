93fead059ed0f9296d775ac3b7f85348785ec7fc
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-06-26 11:28:36 -0300

    Add cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const productRoutes = require("./src/routes/productRoutes");
 const orderRoutes = require("./src/routes/orderRoutes");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const productRoutes = require('./routes/productRoutes');
+const express = require("express");
+const productRoutes = require("./src/routes/productRoutes");
+const orderRoutes = require("./src/routes/orderRoutes");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).send("Hey this is my API running 🥳");
     res.end();
 });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
